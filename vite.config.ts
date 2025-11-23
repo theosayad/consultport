c378043c0b791bcf167993d0d77a2d7c5db7058a
Theo <theosayad@Theo.local>
2025-11-23 04:57:40 +0100

    Configure for custom domain subpath

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,17 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/',  // Keeps your custom domain working
+      // This ensures the app looks for files in /consultport/
+      base: '/consultport/',
+
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
-      // KEYS REMOVED HERE
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
